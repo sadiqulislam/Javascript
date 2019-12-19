d1a6734a2a42932d7bfe798798660fd93d1ca348
Shishir <sishishir2015@gmail.com>
2019-12-19 21:18:21 +0600

    Console Method

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log("Welcome To Console")
+console.log("Welcome To Console");
+console.time("My Code Took");
+console.log(34*4);
+console.log({shishir: 'this',marks:86});
+console.table({shishir: 'this',marks:88});
+console.warn("This is Warning")
+console.error("This Is Huge Errors")
+console.timeEnd("My Code Took")
